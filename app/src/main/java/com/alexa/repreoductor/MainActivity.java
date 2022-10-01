c0d73578387957eb364f68c3d8e6930ca8d51b78
alexf <alex.fantastico22@gmail.com>
2022-09-30 19:16:16 -0500

    Update views

--- app/src/main/java/com/alexa/repreoductor/MainActivity.java
+++ app/src/main/java/com/alexa/repreoductor/MainActivity.java
@@ -1,40 +1,49 @@
 package com.alexa.repreoductor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
-import com.alexa.repreoductor.Data.DataFile;
-import com.google.android.material.tabs.TabLayout;
 
-import java.util.List;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+
         ViewPager2 pager = findViewById(R.id.pager);
 
         pager.setAdapter(new AdapterFragment(getSupportFragmentManager(), getLifecycle()));
 
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.page_1:
+                        pager.setCurrentItem(0, false);
+                        return true;
+                    case R.id.page_2:
+                        pager.setCurrentItem(1, false);
+                        return true;
+                    case R.id.page_3:
+                        pager.setCurrentItem(2, false);
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
-
-    void init(){
-        DataFile dataFile = new DataFile();
-        List<ListElement> lista = dataFile.Song();
-        SongListAdapter songLisAdapter = new SongListAdapter(lista, this);
-        RecyclerView recyclerView = findViewById(R.id.rvSong);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(songLisAdapter);
-    }
-
-
 
 }
